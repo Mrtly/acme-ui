b1a253e7d21c20807644749cf6ec8a69c2f5e6e4
Myrtle <50080618+Mrtly@users.noreply.github.com>
2025-01-29 15:01:35 -0500

    Update preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -12,19 +12,6 @@ const preview: Preview = {
 				date: /Date$/,
 			},
 		},
-		a11y: {
-			//axe-core config options https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#parameters-1
-			//https://react-spectrum.adobe.com/react-aria/Select.html#accessibility
-			//https://react-spectrum.adobe.com/react-aria/accessibility.html#false-positives
-			config: {
-				rules: [
-					{
-						id: 'aria-hidden-focus',
-						selector: 'body *:not([data-a11y-ignore="aria-hidden-focus"])',
-					},
-				],
-			},
-		},
 	},
 }
 
